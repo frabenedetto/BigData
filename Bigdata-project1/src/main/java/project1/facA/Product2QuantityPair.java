6697ad7c5051eae2f140ec1bfaf98577a8127fb2
frabenedettp <francolammi@hotmail.it>
2015-04-15 18:45:55 +0200

    mainb

--- Bigdata-project1/src/main/java/project1/facA/Product2QuantityPair.java
+++ Bigdata-project1/src/main/java/project1/facA/Product2QuantityPair.java
@@ -1,9 +1,14 @@
 package project1.facA;
 
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparable;
 
-public class Product2QuantityPair {
+public class Product2QuantityPair implements WritableComparable<Product2QuantityPair>{
 
 	private Text product;
 	private IntWritable quantity;
@@ -43,6 +48,24 @@ public class Product2QuantityPair {
 
 	public String toString(){
 		return product.toString()+" "+ quantity.toString();
+	}
+
+	public void readFields(DataInput in) throws IOException {
+		product = new Text(in.readUTF());
+		quantity = new IntWritable(Integer.parseInt(in.readUTF()));
+	}
+
+	public void write(DataOutput out) throws IOException {
+		out.writeUTF(product.toString());
+		out.writeUTF(quantity.toString());
+	}
+
+	public int compareTo(Product2QuantityPair arg0) {
+		int cmp = this.product.compareTo(arg0.getProduct());
+		if(cmp==0) {
+			return this.quantity.compareTo(arg0.getQuantity());
+		}
+		return cmp;
 	}
 	
 }
---
+++ Bigdata-project1/src/main/java/project1/facB/MainB.java
@@ -0,0 +1,56 @@
+package project1.facB;
+
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+
+
+public class MainB extends Configured implements Tool{
+
+	public int run(String[] args) throws Exception {
+	    Path input = new Path(args[0]);
+	    Path output = new Path(args[1]);
+	    Configuration conf = getConf();
+	  
+	    Job job1 = new Job(conf, "ex-b");
+	    FileInputFormat.addInputPath(job1, input);
+	    FileOutputFormat.setOutputPath(job1, output);
+	    
+	    job1.setJarByClass(MainB.class);
+	    
+	    job1.setMapperClass(Job1Mapper.class);
+	    job1.setReducerClass(Job1Reducer.class);
+	    
+	    job1.setInputFormatClass(TextInputFormat.class);
+	    
+	    job1.setMapOutputKeyClass(java.util.HashSet.class);
+	    job1.setMapOutputValueClass(IntWritable.class);
+	    boolean succ = job1.waitForCompletion(true);
+	    if (! succ) {
+	      System.out.println("Job1 failed, exiting");
+	      return -1;
+	    }
+	
+	    
+	    return 0;
+
+	  }
+
+	  public static void main(String[] args) throws Exception {
+	    if (args.length != 2) {
+	      System.exit(-1);
+	    }
+	    int res = ToolRunner.run(new Configuration(), new MainB(), args);
+	    System.exit(res);
+	  }
+	
+}
