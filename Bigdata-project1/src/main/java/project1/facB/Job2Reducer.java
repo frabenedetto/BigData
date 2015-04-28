69548ffb6df418239210494d91501afa9ef9bec0
Flavio <flavio.cordari@gmail.com>
2015-04-28 17:22:01 +0200

    ardd

--- Bigdata-project1/src/main/java/project1/facB/Job2Reducer.java
+++ Bigdata-project1/src/main/java/project1/facB/Job2Reducer.java
@@ -12,16 +12,9 @@ public class Job2Reducer extends Reducer<IntWritable, Text, Text, IntWritable>{
 	public void reduce(IntWritable arg0, Iterable<Text> arg1,
 			Context ctx)
 			throws IOException, InterruptedException {
-		String tipo = null;
-		if(arg0.get() == 2)
-			tipo = "coppie";
-		else if(arg0.get() == 3)
-			tipo = "triple";
-		else if(arg0.get() == 4)
-			tipo = "quadruple";
-		int sum = 0;
+		String tipo = new String(Integer.toString(arg0.get()));
 		
-		
+		int sum =0;
 		for(Text t : arg1){
 			sum++;
 		}
